5e16c1174d53ea6cc698fd7ec2f398a04e158659
Genesis Scott <chandlerscott123@gmail.com>
2023-06-28 15:19:40 -0500

    Add conversion support, reads age as string, converts to u8

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
+use std::str::FromStr;
+
 struct Person {
     first_name: String,
     last_name: String,
+    age: u8,
 }
 
 impl Person {
-    fn new(first_name: String, last_name: String) -> Person {
+    fn new(first_name: String, last_name: String, age: u8) -> Person {
         Person {
             first_name,
             last_name,
+            age,
         }
     }
     fn print(&self) {
         println!("Hello {} {}", self.first_name, self.last_name);
+        println!("You are {} years old.", self.age);
     }
 }
 
 fn main() {
     let person = new_from_input();
-    let person2 = Person::new("John".to_string(), "Doe".to_string());
+    let person2 = Person::new("John".to_string(), "Doe".to_string(), 42);
 
     let new_vec = vec![person, person2];
 
@@ -31,7 +36,9 @@ fn new_from_input() -> Person {
     let first_name = read_string();
     println!("What is your last name?");
     let last_name = read_string();
-    let person = Person::new(first_name, last_name);
+    println!("What is your age?");
+    let age = read_number();
+    let person = Person::new(first_name, last_name, age);
     person
 }
 
@@ -42,4 +49,9 @@ fn read_string() -> String {
         .expect("can not read user input");
     let cleaned_input = input.trim().to_string();
     cleaned_input
+}
+
+fn read_number() -> u8 {
+    let input = read_string();
+    u8::from_str(&input).expect("input is not a number")
 }
